0e6f371280e91e0878e5aa2502cfb953961b7d5f
Joanne Nicholson <joanne.s.nicholson@gmail.com>
2020-02-25 14:54:22 -0800

    uploaded

---
+++ Homework/Nodeserver and Express notes.js
@@ -0,0 +1,82 @@
+
+// SERVER SET-UP
+// Require and import the HTTP module
+// Define a port to listen for incoming requests
+// Create a generic function to handle requests and responses
+// Send the string to the client when the user visits the PORT URL
+// Use Node HTTP package to create server 
+// Pass the handleRequest function to empower it with functionality
+// Start server so it can begin listening to client requests
+// Log server side when our server has started 
+
+// TWO SERVERS 
+// Write function for each server
+// Create servers in the same way as one but just repeat
+// Start server...listen 
+// Console log to see callback is triggered when server is successfully listening 
+// Start other server and repeat 
+
+// PORTFOLIO
+// handleRequest function 
+// capture the URL 
+// Switch/case/default to display the HTML file
+// Configure the response to return a status code of 200 (meaning everything went OK), and to be an HTML document
+// End the response by sending the client the myHTML string
+// Also write a function for when a path is not defined - configure response to return a 404
+
+// SERVING HTML 
+// Set dependencies
+// Set port 
+// Create server 
+// Create function for handling req and res 
+// Use fs package to read indexed.html (__dirname)
+// Respond to client with html page by telling browser we are delivering html file 
+// Start server - server.listen
+
+// SERVE FAVOURITES/ URLS 
+// Set dependencies - http and fs 
+// Set port/ handelrequest function 
+// Capture URL 
+// When we visit different URLs read and respond with different files. (Switch path/ case) 
+// Default to rendering indexedDB.html if none of above cases are hit 
+// Start server (start code goes at the end of the page...unintuitively...)
+
+// REQUEST METHODS 
+// Set dependencies 
+// Save request data as a variable
+// Write .on method (on method binds an event to an object ... event driven programming) for when the server receives data 
+// Add it to requestData 
+// End the function 
+// Log the request method and data received 
+
+// POST METHOD 
+// Set dependencies (http, fs, port, create server)
+// Function handleRequest 
+// Switch/case/default 
+// Render function 
+// Save request posted data as a variable. It will be posted to the empty string 
+
+// STAR WARS 
+// Dependencies 
+// Set up Express app (middleware) and port (The express function is a top level function exported by the express module)
+// Set up data parsing - express app handles this 
+// Make data - javascript objects 
+// Route user to the AJAX page:
+//  (app.get then pass in parameters to the function...req and res)
+//  view html then add html 
+//  Then the api - app.get and function res,json with the object 
+//  Can do one or all objects 
+//  For loop the object 
+//  Take in JSON input to create new characters - app.post. 
+//  req.body ... 
+//  RegEx pattern to remove spaces 
+//  Push new characters
+//  Respond with json 
+//  Start server  
+
+ 
+
+
+
+
+
